92fa9974a87ac79ff1c168dbdce955816ec1d594
eganyhlee <eganyhlee@gmail.com>
2020-05-14 02:09:22 -0400

    to grandparents and load in its child

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+	selector: 'app-recipe-item',
+	templateUrl: './recipe-item.component.html',
+	styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
 	@Input() recipe: Recipe;
+	@Output() selectedRecipe = new EventEmitter<Recipe>();
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
+
+	selectItem(selectedRecipe: Recipe) {
+		this.selectedRecipe.emit(selectedRecipe);
+	}
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
 	recipes: Recipe[] = [
@@ -13,9 +13,15 @@ export class RecipeListComponent implements OnInit {
 		new Recipe('Test', 'This is a test Recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
 	];
 
-  constructor() { }
+	@Output() selectedRecipeInList = new EventEmitter<Recipe>();
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	loadRecipeDetail(recipe: Recipe) {
+		this.selectedRecipeInList.emit(recipe);
+	}
 
 }
