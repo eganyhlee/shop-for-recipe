fe9b08a2f0bd809327c68fbcf6d00011aeb95c29
eganyhlee <eganyhlee@gmail.com>
2020-05-18 14:42:26 -0400

    cross service comm

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,43 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class RecipeService {
+
 	recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-		new Recipe('Test', 'This is a test Recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'),
-		new Recipe('Test1', 'This is a test Recipe1', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
+		new Recipe(
+			'Test',
+			'This is a test Recipe',
+			'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg',
+			[
+				new Ingredient('Meat', 1),
+				new Ingredient('Egg', 2)
+			]
+		),
+		new Recipe(
+			'Test1',
+			'This is a test Recipe1',
+			'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg',
+			[
+				new Ingredient('Chicken', 1),
+				new Ingredient('Onion', 2)
+			]
+		)
 	];
 
-	getRecipes(){
+	getRecipes() {
 		return this.recipes.slice(); //get a copy, not itself
 	}
-	
-  constructor() { }
+
+	constructor(private slService: ShoppingListService) { }
+
+	addIngredientsToShoppingList(ingredients: Ingredient[]) {
+		this.slService.addIngredientList(ingredients);
+	}
 }
