c0d6a5912cd35dcfe0340eb78be7418bf5d67fd0
eganyhlee <eganyhlee@gmail.com>
2020-05-19 14:54:45 -0400

    editing exisiing data

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -7,7 +7,8 @@ import { Ingredient } from '../shared/ingredient.model';
   providedIn: 'root'
 })
 export class ShoppingListService {
-	@Output() ingredientChanged = new Subject<Ingredient[]>();
+	ingredientChanged = new Subject<Ingredient[]>();
+	startedEditing = new Subject<number>();
 	
 	private ingredients: Ingredient[] = [
 		new Ingredient('Apples', 5),
@@ -20,6 +21,10 @@ export class ShoppingListService {
 		return this.ingredients.slice();
 	}
 
+	getIngredient(index: number) {
+		return this.ingredients[index];
+	}
+
 	addIngredient(ingredient: Ingredient) {
 		this.ingredients.push(ingredient);
 		this.ingredientChanged.next(this.ingredients.slice());
@@ -27,6 +32,11 @@ export class ShoppingListService {
 
 	addIngredientList(ingredients: Ingredient[]) {
 		this.ingredients.push(...ingredients);
+		this.ingredientChanged.next(this.ingredients.slice());
+	}
+
+	updateIngredient(index:number, newIngredient: Ingredient) {
+		this.ingredients[index] = newIngredient;
 		this.ingredientChanged.next(this.ingredients.slice());
 	}
 }
